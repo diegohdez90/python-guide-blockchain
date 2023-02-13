1873ad6c5392388d02aa7ecc1e7449872bf3f497
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2023-02-13 17:42:20 -0600

    feat: update verification transaction

--- blockchain.py
+++ blockchain.py
@@ -118,8 +118,6 @@ class Blockchain:
         if self.hosting_node == None:
             return False
         transaction = Transaction(sender, recipient, signature, amount)
-        if not Wallet.verify_transaction(transaction):
-            return False
         if Verification.verify_transaction(transaction, self.get_balance):
             self.__open_transactions.append(transaction)
             self.save_data()
@@ -136,12 +134,12 @@ class Blockchain:
         reward_transaction = Transaction(
             'MINING', self.hosting_node, '', MINING_REWARD)
         copied_transaction = self.__open_transactions[:]
+        for tx in copied_transaction:
+            if not Wallet.verify_transaction(tx):
+                return False
         copied_transaction.append(reward_transaction)
         block = Block(len(self.__chain), hashed_block,
                       copied_transaction, proof)
-        for tx in block.transactions:
-            if not Wallet.verify_transaction(tx):
-                return False
         self.__chain.append(block)
         self.__open_transactions = []
         self.save_data()
