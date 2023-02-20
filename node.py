a310bd1e06468ee350d0b11f23442d6703c56d89
Diego Arturo Hernandez Fuentes <diegohdez12@gmail.com>
2023-02-20 13:10:50 -0600

    feat: add block request to receiver user

--- node.py
+++ node.py
@@ -101,12 +101,44 @@ def post_broadcasting_transaction():
                 'amount': values['amount'],
                 'signature': values['signature']
             },
-            'funds': blockchain.get_balance()
         }
         return jsonify(response), 200
     else:
         response = {
             'message': "transaction failed"
+        }
+        return jsonify(response), 400
+
+
+@app.route("/broadcast-block", methods=["POST"])
+def broadcast_block():
+    values = request.get_json()
+    if not values:
+        response = {
+            'message': "No data found"
+        }
+        return jsonify(response), 400
+    if 'block' not in values:
+        response = {
+            'message': "Data is missing"
+        }
+        return jsonify(response), 400
+    block = values["block"]
+    if block['index'] == blockchain.chain[-1].index + 1:
+        if blockchain.add_block(block):
+            response = {
+                'meesage': 'Block added'
+            }
+            return jsonify(response), 201
+        response = {
+            'message': 'Block seems invalid'
+        }
+        return jsonify(response), 400
+    elif block['index'] > blockchain.chain[-1].index:
+        pass
+    else:
+        response = {
+            'message': 'Blockchain seems to be shorter, block not added'
         }
         return jsonify(response), 400
 
